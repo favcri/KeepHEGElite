b7f7415de051f4dd752516c4f0a012d3ccb989f0
favcri <favell.cristian81@gmail.com>
2021-04-18 23:20:52 +0200

    Fichier initiaux pour un splash

---
+++ app/src/main/java/com/devmobile/keephegelite/common/activities/SplashScreen.java
@@ -0,0 +1,28 @@
+package com.devmobile.keephegelite.common.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.devmobile.keephegelite.R;
+import com.devmobile.keephegelite.recyclerview.MainActivity;
+
+public class SplashScreen extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_splash);
+
+		new Handler().postDelayed(new Runnable() {
+			@Override
+			public void run(){
+				Intent spIntent = new Intent(SplashScreen.this, MainActivity.class);
+				startActivity(spIntent);
+				finish();
+			}
+		}, 3000);
+	}
+}
